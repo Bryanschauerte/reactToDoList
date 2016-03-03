da7906bba9e44e2dfd311f1b150ac4e1c66694e4
Bryan Schauerte <bryan.schauerte@gmail.com>
2016-03-03 09:22:30 -0700

    webpack set up, still need to describe it for later readers

---
+++ app/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+var AppTODO = React.createClass({
+
+  render(){
+    return (<div>works
+    </div>
+    );
+  }
+});
+ReactDOM.render(<AppTODO/>, document.getElementById('target'));
